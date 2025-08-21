e73552ae880c284f690a4b187186d753fb21156d
AmarnathSooraj <amarnathsooraj7575@gmail.com>
2025-08-22 00:05:30 +0530

    added

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-let login = document.getElementById('loginform');
+let inputemail = document.getElementById('inputemail');
+let formemail = document.getElementById('formemail');
 let checkbox = document.getElementById('checkbox');
-let pass = document.getElementById('pass');
-let formlogin = document.getElementById('formlogin')
+let inputpass = document.getElementById('pass');
+let content = document.getElementById('content');
+
+formemail.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    content.innerText = inputemail.value;
+})
 
 checkbox.addEventListener('click',()=>{
-    if(pass.type==='text'){
-        pass.type='password'
+    if(inputpass.type==='text'){
+        inputpass.type='password'
     }
     else{
-        pass.type='text' 
+        inputpass.type='text' 
     }
 })
 
